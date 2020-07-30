5c0b40239f0954786becccf6065e7991dee4ea7d
Jorge A. Pérez Garay <jperez@sw1848.com>
2020-07-30 08:53:50 -0500

    Read and update completed

--- demo_graphql_python/api/schema.py
+++ demo_graphql_python/api/schema.py
@@ -13,3 +13,28 @@ class Query(graphene.ObjectType):
 
     def resolve_students(self, info, **kwargs):
         return Student.objects.all()
+
+"""StudentMutation"""
+class StudentMutation(graphene.Mutation):
+    class Arguments:
+        # The input arguments for this mutation
+        name = graphene.String()
+        email = graphene.String()
+        phone = graphene.String()
+        id = graphene.ID()
+    
+    # The class attributes define the response of the mutation
+    student = graphene.Field(StudentType)
+
+    def mutate(self, info, name, email, phone, id=None):
+        student = Student.objects.get(pk=id)
+        student.name = name
+        student.email = email
+        student.phone = phone
+        student.save()
+
+        return StudentMutation(student=student)
+
+"""Some coment"""
+class Mutation(graphene.ObjectType):
+    update_student = StudentMutation.Field()
--- demo_graphql_python/schema.py
+++ demo_graphql_python/schema.py
@@ -8,4 +8,9 @@ class Query(schema.Query, graphene.ObjectType):
     # as we begin to add more apps to our project
     pass
 
-schema = graphene.Schema(query=Query)
+class Mutation(schema.Mutation, graphene.ObjectType):
+    # This class will inherit from multiple Queries
+    # as we begin to add more apps to our project
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
